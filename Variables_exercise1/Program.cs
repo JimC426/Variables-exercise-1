74a017f8cf223be40bf39051d43ca97220557a69
Jim Corbett <jcorbett426@gmail.com>
2023-11-16 11:36:06 -0500

    completed variables exercise

---
+++ Variables_exercise1/Program.cs
@@ -0,0 +1,18 @@
+﻿// Declare and initialize variables
+string name = "Jim";
+int age = 23;
+char middleInitial = 'J';
+bool isOver18 = true;
+double currentTemp = 51.9;
+decimal currentPrice = 29.99m;
+
+// Writing the variables to the console
+Console.WriteLine($"Name: {name}");
+Console.WriteLine($"Age: {age}");
+Console.WriteLine($"Middle Initial: {middleInitial}");
+Console.WriteLine($"Is Over 18: {isOver18}");
+Console.WriteLine($"Current Temperature: {currentTemp}");
+Console.WriteLine($"Current Price: {currentPrice}");
+
+// Made a story using variables.
+Console.WriteLine($"My name is {name}. I am {age} years old. My middle initial is {middleInitial}. The current temperature is {currentTemp} degrees. I have {currentPrice} dollars.");
